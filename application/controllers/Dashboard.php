ce4a631cdd633fc4d745b5131b94ae97c9cecc79
Nur Ihsan <48948128+SDIhsan@users.noreply.github.com>
2020-10-09 17:45:11 +0700

    ss

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,2 +1,15 @@
 <?php
 defined('BASEPATH') or exit('No direct access script allowed');
+
+class Dashboard extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        //kemenet
+    }
+}
